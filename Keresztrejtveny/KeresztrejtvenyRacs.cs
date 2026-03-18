1b0e3f99a3bb750adb8fae18f339e61c02439c3a
Petrik Diak <petrik.diak@petrik.hu>
2026-03-18 08:25:40 +0100

    Prpoperty-k és field-ek készek 1 feladat kész, valamint a másodiknak a fele.

---
+++ Keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keresztrejtveny
+{
+	internal class KeresztrejtvenyRacs
+	{
+		private List<string> Adatsorok;
+		private char[,] Racs;
+		private int[,] Sorszamok;
+
+		public int SorokDb { get; }
+		public int OszlopokDb { get; }
+	}
+}
