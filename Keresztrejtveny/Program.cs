e98c6b35dc6730846bb6d98cdd566ace8b5c33b2
Petrik Diak <petrik.diak@petrik.hu>
2026-03-18 09:03:13 +0100

    5. feladatnál sorok befelé tolása

--- Keresztrejtveny/Program.cs
+++ Keresztrejtveny/Program.cs
@@ -7,11 +7,11 @@
             KeresztrejtvenyRacs racs = new KeresztrejtvenyRacs("kr1.txt");
 
 			Console.WriteLine("5. feladat: A keresztrejtvény mérete");
-			Console.WriteLine($"Sorok száma: {racs.SorokDb}");
-			Console.WriteLine($"Oszlopok száma: {racs.OszlopokDb}");
+			Console.WriteLine($"\tSorok száma: {racs.SorokDb}");
+			Console.WriteLine($"\tOszlopok száma: {racs.OszlopokDb}");
 
 			Console.WriteLine("6. feladat: A beolvasott keresztrejtvény");
-            racs.Megjelenit();
+			racs.Megjelenit();
 
 			Console.WriteLine($"7. feladat: A leghosszabb függ.: {racs.LeghosszabbFuggoleges()} karakter");
 		}
