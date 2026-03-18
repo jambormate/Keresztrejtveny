b925f2e06aa7dff1c7f90f4d40e4d326e098f212
Petrik Diak <petrik.diak@petrik.hu>
2026-03-18 08:36:49 +0100

    3. feladat egésze, valamint a 2. feladat maradéka kész

--- Keresztrejtveny/KeresztrejtvenyRacs.cs
+++ Keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -14,5 +14,45 @@ namespace Keresztrejtveny
 
 		public int SorokDb { get; }
 		public int OszlopokDb { get; }
+
+		public KeresztrejtvenyRacs(string forras)
+		{
+			BeolvasAdatsorok(forras);
+
+			SorokDb = Adatsorok.Count;
+			OszlopokDb = Adatsorok[0].Length;
+
+			Racs = new char[SorokDb + 2, OszlopokDb + 2];
+			Sorszamok = new int[SorokDb + 2, OszlopokDb + 2];
+
+			FeltoltRacs();
+		}
+
+		public void BeolvasAdatsorok(string forras)
+		{
+			Adatsorok = new List<string>(File.ReadAllLines(forras));
+		}
+
+		public void FeltoltRacs()
+		{
+			for (int i = 0; i < SorokDb; i++)
+				for (int j = 0; j < OszlopokDb; j++)
+					Racs[i + 1, j + 1] = Adatsorok[i][j];
+		}
+
+		public void Megjelenit()
+		{
+			for (int i = 1; i <= SorokDb; i++)
+			{
+				for (int j = 1; j <= OszlopokDb; j++)
+				{
+					if (Racs[i, j] == '-')
+						Console.Write("[]");
+					else
+						Console.Write("##");
+				}
+				Console.WriteLine();
+			}
+		}
 	}
 }
